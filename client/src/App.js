c42cbab3777130062eb4bd29caa5a5b482704f3f
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-24 11:48:10 -0800

    add client main component

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 
-function App() {
+import NavBar from './components/NavBarComp';
+import Main from './components/MainComp'
+
+////////////////////////////////////////////////////
+//  ROOT APPLICATION
+////////////////////////////////////////////////////
+
+/**
+ * Root Application
+ * @returns HTML for the Root Application
+ */
+const App = () => {
+  // //// OUTPUT ///////////////////////////////////
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        {/* **** NAVIGATION BAR ********* */}
+        <NavBar />
+        <Switch >
+          {/* **** MAIN COMPONENT ********** */}
+          <Main />
+        </Switch>
+      </BrowserRouter>
   );
 }
 
