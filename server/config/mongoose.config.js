ce26a1c9e0f14d21be02a989c64ce6edc48a8b28
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-24 11:39:54 -0800

    implement index.html

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -11,5 +11,5 @@ mongoose.connect("mongodb://localhost/" + DATABASE_NAME, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 })
-	.then(() => console.log("🌮🌮🌮 Established a connection to the database 🌮🌮🌮"))
+	.then(() => console.log("🌈🌈🌈 Established a connection to the database 🌈🌈🌈"))
 	.catch(err => console.log(`🤷🤷🤷 Something went wrong when connecting to the database 🤷🤷🤷`, err));
