0e522eda0f8c7d6b4a34135b6fcb5779cc7c06c9
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-24 11:20:00 -0800

    add /client

--- server/server.js
+++ server/server.js
@@ -11,10 +11,10 @@ const PORT = 8000;
 app.use(express.json(), express.urlencoded({ extended: true }));
 
 // This will fire our mongoose.connect statement to initialize our database connection
-require("./server/config/mongoose.config");
+require("./config/mongoose.config");
 
 // This is where we import the users routes function from our user.routes.js file
-const AllMyJokeRoutes = require("./server/routes/joke.routes");
+const AllMyJokeRoutes = require("./routes/joke.routes");
 AllMyJokeRoutes(app);
 
 app.listen(PORT, () => console.log(`🦄🦄🦄 The server is all fired up on port ${PORT} 🦄🦄🦄`));
